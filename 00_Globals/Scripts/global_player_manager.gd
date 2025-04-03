2278e487d4ce3c15321a4120056ddaac0f301c65
robgriffin247 <robgriffin247@hotmail.com>
2025-04-03 08:40:23 +0200

    13 - player spawn scene added

--- 00_Globals/Scripts/global_player_manager.gd
+++ 00_Globals/Scripts/global_player_manager.gd
@@ -1,3 +1,31 @@
 extends Node
 
+# Preload the player scene
+const PLAYER = preload("res://Player/Scenes/player.tscn")
+
 var player: Player
+var player_spawned = false
+
+
+func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.2).timeout
+	player_spawned = true
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+
+
+func set_player_position(_new_position: Vector2) -> void:
+	player.global_position = _new_position
+
+
+func set_as_parent(_parent: Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	_parent.add_child(player)
+
+
+func unparent_player(_parent: Node2D):
+	_parent.remove_child(player)
